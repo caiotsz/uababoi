75bfbda4485839c891a29e9c56c9e700ab769ce9
caiotsz <161315259+caiotsz@users.noreply.github.com>
2024-10-15 11:30:39 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,121 @@
+
+    criaCartao(
+    'Português',
+    'O que é um substantivo?',
+    'Uma palavra que nomeia coisas, pessoas ou lugares.'
+    )
+    
+    criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'O processo em que as plantas produzem alimento usando luz solar.'
+    )
+    
+    criaCartao(
+    'História',
+    'Quem foi Cristóvão Colombo?',
+    'Um navegador que descobriu a América em 1492.'
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília'
+    )
+    
+    criaCartao(
+    'Matemática',
+    'O que é um triângulo?',
+    'Uma figura com três lados.'
+    )
+    
+    criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Uma palavra que indica ação ou estado.'
+    )
+    
+    criaCartao(
+    'Ciências',
+    'O que é a água?',
+    'Um líquido essencial para a vida, composto por hidrogênio e oxigênio.'
+    )
+    
+    criaCartao(
+    'História',
+    'Quando aconteceu a Revolução Francesa?',
+    'Em 1789.'
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'Ásia'
+    )
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a multiplicação de 3 x 3?',
+    '9'
+    )
+    
+    criaCartao(
+    'Português',
+    'O que é uma frase?',
+    'Um conjunto de palavras que expressa uma ideia completa.'
+    )
+    
+    criaCartao(
+    'Ciências',
+    'O que é um ecossistema?',
+    'Um conjunto de seres vivos e o ambiente em que vivem.'
+    )
+    
+    criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Um líder militar e imperador francês.'
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio Nilo.'
+    )
+    
+    criaCartao(
+    'Matemática',
+    'O que é uma fração?',
+    'Uma parte de um todo, representada por dois números.'
+    )
+    
+    criaCartao(
+    'Português',
+    'O que é uma poesia?',
+    'Um texto que expressa sentimentos e ideias de forma artística.'
+    )
+    
+    criaCartao(
+    'Ciências',
+    'O que é um átomo?',
+    'A menor unidade de um elemento químico.'
+    )
+    
+    criaCartao(
+    'História',
+    'Quando ocorreu a Segunda Guerra Mundial?',
+    'De 1939 a 1945.'
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual é a maior montanha do mundo?',
+    'O Monte Everest.'
+    )
+    
+    criaCartao(
+    'Matemática',
+    'O que é um número par?',
+    'Um número que pode ser dividido por 2 sem deixar resto.'
+    )
+    
